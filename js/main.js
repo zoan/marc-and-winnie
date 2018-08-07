3c38511a0c5fa6654142f880dd62834e0fbdd7d6
Marc Wong <marcdwong@gmail.com>
2018-08-06 22:13:28 -0700

    mobile styling update, update fullpage order

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
 		//Navigation
 		menu: '#menu',
 		lockAnchors: false,
-		anchors:['hero', 'venue', 'our-story', 'wedding-party', 'rsvp', 'registry', 'last'],
+		anchors:['hero', 'venue', 'our-story', 'wedding-party', 'registry', 'rsvp', 'last'],
 		navigation: true,
 		navigationPosition: 'right',
 		navigationTooltips: [],
